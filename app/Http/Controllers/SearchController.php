f80512cb5b60e493f1806f6a08b36dcd286d9c99
Nenad Ticaric <nticaric@gmail.com>
2017-09-01 00:50:31 +0200

    blank line

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -27,7 +27,6 @@ class SearchController extends Controller
             'didyoumean' => true,
             'data'       => $this->getSuggestions($query)
         ];
-
     }
 
     public function isExactMatch($request, $result)
